64188f019eda5353cc6b5b5bb9af7f5da4159786
huy <phambahuy.cv@gmail.com>
2026-03-31 16:37:06 +0700

    update data.yaml and model

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import cv2
 model = YOLO("runs/detect/train/weights/best.pt")
 
 
-results = model("dataset/valid/images/diote.jpg", show=True)
+results = model("dataset/test/images/capacitors-2-_jpg.rf.0385cd49a870faa91ece9498ec91e825.jpg", show=True)
 
 
 img = results[0].plot()
